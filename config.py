be26e72c07003644596ba94e6d153dbb78aee42a
Bohdan Turani <bodya00@list.ru>
2019-06-18 22:31:37 +0200

    Add string parsing

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 company_address = 'https://en.wikipedia.org/wiki/Tesla,_Inc.'
 exchange_currency = {'name': 'DKK', 'exchange_rate': 6.472}
+multiplicators = {'billion': 10 ** 9, 'million': 10 ** 6, '': 1}
 params_to_extract = ['operating income', 'revenue', 'net income',
                      'total assets', 'total equity']
 # all parameters to extract must have same name as on Wikipedia page.
--- tesla_scraper.py
+++ tesla_scraper.py
@@ -1,6 +1,10 @@
-from typing import List, Dict, Union
+import re
+from typing import Union, List, Dict
 
-from config import company_address, params_to_extract, exchange_currency
+import bs4 as bs
+import requests
+
+from config import multiplicators
 
 Number = Union[int, float]
 
@@ -10,7 +14,32 @@ Currently only info from summary(left table on Wiki) is supported.
 """
 
 
-def get_params(adress: str, params: List[str]) -> Dict[str, Number]:
+def parse_param_string(parameter_string):
+    """
+    Get number from string of format {currency}{value}{multiplicator}
+    """
+    parameter_string = parameter_string.replace('\xa0', ' ')
+
+    match = re.search(
+            r'(?P<currency>US\$)?'  # currency
+            r'(?P<value>[+-]?\d+(?:\.\d+)?)'  # value
+            r'(?P<multiplicator>\s[a-zA-Z]*)?',  # billion or million
+            parameter_string)
+
+    if not match:
+        raise ValueError('Parameter is not numeric')
+
+    currency, value, multiplicator = match.groups(default='')
+    if not currency:
+        raise ValueError('Bad currency')
+    if multiplicator not in multiplicators:
+        raise ValueError('Bad multiplicator')
+    multiplicator = multiplicator[1:]  # eliminating required space char
+
+    return float(value) * multiplicators[multiplicator]
+
+
+def scrape_params(html: str, params: List[str]) -> Dict[str, Number]:
     """
     Get company parameters from its Wikipedia page.
     """
@@ -20,6 +49,11 @@ def get_params(adress: str, params: List[str]) -> Dict[str, Number]:
 
 def get_company_name(address: str) -> str:
     return 'Tesla'
+
+
+def download_page(address: str) -> str:
+    """Right now it looks more like an alias, but in future way of downloading html_content may be changed"""
+    return requests.get(address).text
 
 
 def convert(params: Dict[str, Number], exchange_rate: Number) -> Dict[str, Number]:
@@ -37,7 +71,8 @@ def output(company_name, params: Dict[str, Number]) -> None:
 
 
 if __name__ == '__main__':
-    params = get_params(company_address, params_to_extract)
+    page = download_page(company_address)
+    params = scrape_params(page, params_to_extract)
     company_name = get_company_name(company_address)
     params = convert(params, exchange_currency)
     output(company_name, params)
